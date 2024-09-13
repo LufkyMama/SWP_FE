bd93cd737c02f53a72aab6347cc31d582071f3b9
LuuTienKhoa <140997759+LuuTienKhoa@users.noreply.github.com>
2024-09-13 15:51:04 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Login from './Login';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -28,7 +29,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="App">
+      <Login />
+    </div>
     </>
+    
   )
 }
 
