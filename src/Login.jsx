2407d2d1c77980569af7025d3918a64f8ce9f753
LuuTienKhoa <140997759+LuuTienKhoa@users.noreply.github.com>
2024-09-15 17:06:47 +0700

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,35 +1,36 @@
 import { Link } from 'react-router-dom';
 import './Login.css';
-    
-const Login = () => {
+
+function Login() {
     return (
-        <div>
-            <div>
-                <h1>Login</h1>
+        <div className="login-container">
+            <div className="login-form">
+                <h1 className="form-title">Login</h1>
                 <form action="">
-            <div>
-                <input type="email" placeholder="Email" />
-                <label htmlFor="email">Email</label>    
+                    <div className="input-group">
+                        <input type="email" className="input-field" placeholder="Your Email" required />
+                        
+                    </div>
+                    <div className="input-group">
+                        <input type="password" className="input-field" placeholder="Your Password" required />
+                        
+
+                    </div>
+                    <div className='options'>
+                        <div className="checkbox-group">
+                            <input type="checkbox" name="checkbox" id="checkbox" />
+                        <label htmlFor="checkbox">Remember me</label>
+                        </div>
+                        <span className="forgot-password">Forgot Password?</span>
+                    </div>
+                    <button type="submit" className="submit-button">Login</button>
+                    <div className="register-link">
+                        <span>Don&apos;t have an account? <Link to='/Register'>Create an account</Link></span>
+                    </div>
+                </form>
             </div>
-            <div>
-                <input type="password" placeholder="Password" />
-                <label htmlFor="password">Password</label>    
-            </div>
-            <div>
-                <div>
-                <input type="checkbox" name="checkbox" id="checkbox" />
-                <label htmlFor="checkbox">Remember me</label>
-                </div>
-                <span>Forgot Password?</span>
-            </div>
-            <button type="Submit">Login</button>
-            <div>
-                <span>Don&apos;t have an account? <Link to='Register'>Create an account</Link></span>
-            </div>
-            </form>
         </div>
-      </div>  
     );
-};
+}
 
 export default Login;
